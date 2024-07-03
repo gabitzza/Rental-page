9bf42544ea6881acb631e7f4aa422e01e63e95f9
Gabriela Marian <138035379+gabitzza@users.noreply.github.com>
2024-07-03 13:23:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+function myFunction() {
+    var x = document.getElementById("navi-bar");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunction1() {
+  var x = document.getElementById("drop-content1");
+  if (x.style.display === "none") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }
+}
+
+function myFunction2() {
+    var x = document.getElementById("drop-content2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+  }
+
+  function myFunction3() {
+    var x = document.getElementById("drop-content3");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+  }
+
+  function myFunction4() {
+    var x = document.getElementById("drop-content4");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+  }
+
+  function toggleDisplay(id) {
+    var x = document.getElementById(id);
+    x.classList.toggle("d-none");
+  }
+
+  function myFunction6() {
+    var x = document.getElementById("drop-content6");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+  }
+
+  function myFunction7() {
+    var x = document.getElementById("drop-content7");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+  }
+
+  function myFunction8() {
+    var x = document.getElementById("drop-content8");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+  }
+
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
